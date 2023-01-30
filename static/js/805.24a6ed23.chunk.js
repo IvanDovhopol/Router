0458ee5d791a215c6b0291c69b77b1158d607171
IvanDovhopol <104319381+IvanDovhopol@users.noreply.github.com>
2023-01-30 09:12:08 +0000

    Deploying to gh-pages from @ IvanDovhopol/Router@124d49d68e39f6437f1305509717c8ed031de299 🚀

---
+++ static/js/805.24a6ed23.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrouter=self.webpackChunkrouter||[]).push([[805],{805:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
+//# sourceMappingURL=805.24a6ed23.chunk.js.map
